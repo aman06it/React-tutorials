b5038c9af1abc89f9a8aece6d5903d3cedd66aa9
Aman Singh6 <aman.singh6@olacabs.com>
2022-07-09 14:31:46 +0530

    event handling in functional ans class component.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import Welcome from './components/Welcome';
 import Hello from './components/Hello'
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 
 function App() {
   return (
     <div className="App">
-      
-      <Counter/>
+      <ClassClick/>
+      {/* <FunctionClick/> */}
+      {/* <Counter/> */}
       {/* <Message /> */}
 
       {/* <Greet name='Aman'>
