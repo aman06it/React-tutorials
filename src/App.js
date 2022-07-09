7d4d316ef419684a1dffe6a0a22adc93e323e622
Aman Singh6 <aman.singh6@olacabs.com>
2022-07-09 14:22:59 +0530

    code for setState

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome';
 import Hello from './components/Hello'
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 
 function App() {
   return (
     <div className="App">
       
-      <Message />
+      <Counter/>
+      {/* <Message /> */}
 
       {/* <Greet name='Aman'>
         <p>children props</p>
